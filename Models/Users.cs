dd6c2c72c10bd18653e9ede4c70e1674cb6dc68d
jmsjcmc <jecemecojamestabilog@gmail.com>
2025-01-02 13:42:24 +0800

    Update: Added users model

---
+++ Models/Users.cs
@@ -0,0 +1,15 @@
+﻿namespace IMS_Api.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string? Firstname { get; set; }
+        public string? Middlename { get; set; }
+        public string? Lastname { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Status { get; set; }
+        public DateTime? Datecreation { get; set; }
+    }
+}
